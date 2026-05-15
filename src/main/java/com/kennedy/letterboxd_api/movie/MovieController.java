a86659e97f1083a441a8a33d047b66b247044c92
Tobechukwu Kennedy Eziolise <tobechukwueziolise@gmail.com>
2026-05-15 09:49:51 -0400

    feat: add ResponseEntity with proper HTTP status codes to MovieController

--- src/main/java/com/kennedy/letterboxd_api/movie/MovieController.java
+++ src/main/java/com/kennedy/letterboxd_api/movie/MovieController.java
@@ -2,6 +2,7 @@ package com.kennedy.letterboxd_api.movie;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,43 +22,51 @@ public class MovieController {
 	private final MovieService movieService;
 	// Spring
 	@GetMapping
-	public List<Movie> getAllMovies() {
-	    return movieService.getAllMovies();
+	public ResponseEntity<List<Movie>> getAllMovies() {
+	    List<Movie> allMovies = movieService.getAllMovies();
+		return ResponseEntity.status(200).body(allMovies);
 	}
 	
 	@GetMapping("/{id}")
-	public Movie getMovie(@PathVariable Long id) {
-		return movieService.getMovie(id);
+	public ResponseEntity<Movie> getMovie(@PathVariable Long id) {
+		Movie gottenMovie = movieService.getMovie(id);
+		return ResponseEntity.status(200).body(gottenMovie);
 	}
 	
 	@PostMapping
-	public Movie createMovie(@RequestBody Movie movie) {
-		return movieService.createMovie(movie);
+	public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+		Movie created = movieService.createMovie(movie);
+		return ResponseEntity.status(201).body(created);
 	}
 	
 	@PutMapping("/{id}")
-	public Movie updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
-		return movieService.updateMovie(id, movie);
+	public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
+		Movie updated = movieService.updateMovie(id, movie);
+		return ResponseEntity.status(200).body(updated);
 	}
 	
 	@DeleteMapping("/{id}")
-	public void deleteMovie(@PathVariable Long id) {
+	public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
 		movieService.deleteMovie(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 	@GetMapping("/search/title")
-	public List<Movie> searchByTitle(@RequestParam String query) {
-		return movieService.searchByTitle(query);
+	public ResponseEntity<List<Movie>> searchByTitle(@RequestParam String query) {
+		List<Movie> title = movieService.searchByTitle(query);
+		return ResponseEntity.status(200).body(title);
 	}
 	
 	@GetMapping("/search/director")
-	public List<Movie> searchByDirector(@RequestParam String query) {
-		return movieService.searchByDirector(query);
+	public ResponseEntity<List<Movie>> searchByDirector(@RequestParam String query) {
+		List<Movie> director = movieService.searchByDirector(query);
+		return ResponseEntity.status(200).body(director);
 	}
 	
 	@GetMapping("/search/genre")
-	public List<Movie> searchByGenre(@RequestParam String query) {
-		return movieService.searchByGenre(query);
+	public ResponseEntity<List<Movie>> searchByGenre(@RequestParam String query) {
+		List<Movie> genre = movieService.searchByGenre(query);
+		return ResponseEntity.status(200).body(genre);
 	}
 	
 }
