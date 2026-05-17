5c18a04af0e42cbf9382106306d0d638b3e26115
Tobechukwu Kennedy Eziolise <tobechukwueziolise@gmail.com>
2026-05-17 15:44:05 -0400

    feat: add duplicate prevention to TMDB seeder, switch ddl-auto to update from create-drop

--- src/main/java/com/kennedy/letterboxd_api/tmdb/TmdbSeeder.java
+++ src/main/java/com/kennedy/letterboxd_api/tmdb/TmdbSeeder.java
@@ -19,19 +19,21 @@ public class TmdbSeeder implements CommandLineRunner {
 	private String apiKey;
 	@Override
     public void run(String... args) throws Exception {
-		String url = String.format("https://api.themoviedb.org/3/movie/popular?api_key=%s&page=1", apiKey);
-		TmdbMovieResponse response = restTemplate.getForObject(url,TmdbMovieResponse.class);
-		for (TmdbMovie tmdbMovie : response.getResults()) {
-			Movie movie = new Movie();
-			movie.setTitle(tmdbMovie.getTitle());
-			movie.setLanguage(tmdbMovie.getOriginalLanguage());
-			movie.setPosterUrl(tmdbMovie.getPosterPath());
-			movie.setReleaseYear(Integer.parseInt(tmdbMovie.getReleaseDate().substring(0, 4)));
-			movie.setRating(tmdbMovie.getVoteAverage());
-			movie.setDirector("Unknown");
-			movie.setGenre("Unknown");
-			movie.setRuntime(0);
-			movieRepository.save(movie);	
+		if(movieRepository.count() == 0) {
+			String url = String.format("https://api.themoviedb.org/3/movie/popular?api_key=%s&page=1", apiKey);
+			TmdbMovieResponse response = restTemplate.getForObject(url,TmdbMovieResponse.class);
+			for (TmdbMovie tmdbMovie : response.getResults()) {
+				Movie movie = new Movie();
+				movie.setTitle(tmdbMovie.getTitle());
+				movie.setLanguage(tmdbMovie.getOriginalLanguage());
+				movie.setPosterUrl(tmdbMovie.getPosterPath());
+				movie.setReleaseYear(Integer.parseInt(tmdbMovie.getReleaseDate().substring(0, 4)));
+				movie.setRating(tmdbMovie.getVoteAverage());
+				movie.setDirector("Unknown");
+				movie.setGenre("Unknown");
+				movie.setRuntime(0);
+				movieRepository.save(movie);	
+			}
 		}
     }
 }
